b289c57b2aec517b336ed6992e5451814d80b5ff
jazzmoe <mhemmerlein@gmx.de>
2017-04-05 23:53:20 +0200

    import and tidy election stats

--- weightingdata.R
+++ weightingdata.R
@@ -34,17 +34,30 @@ rm(p, packages)
 
 ###################################
 
-# Get data
+# Get data Bundeswahlstatistik
+# Wähler der Parteien nach Alter und Geschlecht
 colnames <- c("Parties",
-              "age_1_13", "age_1_09",
-              "age_2_13", "age_2_09",
-              "age_3_13", "age_3_09",
-              "age_4_13", "age_4_09",
-              "age_5_13", 
-              "age_6_13", "age_6_09",
-              "age_7_13")
+              "age18_25_2013", "age18_25_2009",
+              "age26_35_2013", "age26_35_2009",
+              "age36_45_2013", "age36_45_2009",
+              "age46_60_2013", "age46_60_2009",
+              "age61_69_2013", 
+              "age60+_2013", "age60+_2009",
+              "age70+_2013")
 
+types <- replicate(13, "numeric")
+types[1] <- "factor"
 
-Wahlstatistik <- read.table("C:\\Users\\Moritz\\Desktop\\ElectionForecasting\\Weitere Daten\\Bundeswahlstatistik\\Wählerschaft_der_Parteien_nach_Geschlecht_und_Alter.csv", 
-                            sep = ";",dec =".", skip = 6)
-row
+Wahlstatistik <- read.csv2("C:\\Users\\Moritz\\Desktop\\ElectionForecasting\\Weitere Daten\\Bundeswahlstatistik\\Wählerschaft_der_Parteien_nach_Geschlecht_und_Alter.csv", 
+                            sep = ";",dec =",", skip = 6, stringsAsFactors = FALSE)
+Wahlstatistik <- Wahlstatistik[c(4:11,13:20,22:29),]
+colnames(Wahlstatistik) <- colnames
+Wahlstatistik$gndr <- c("Total", "Total", "Total", "Total", "Total", "Total", "Total", "Total",
+                        "Male", "Male", "Male", "Male", "Male", "Male", "Male", "Male",
+                        "Female", "Female", "Female", "Female", "Female", "Female", "Female", "Female")
+Wahlstatistik <- select(Wahlstatistik, gndr, everything())
+Wahlstatistik[3] <- as.numeric(sub(",", ".", unlist(Wahlstatistik[3]), fixed = TRUE))
+Wahlstatistik[1] <- as.factor(unlist(Wahlstatistik[1]))
+Wahlstatistik[2] <- as.factor(unlist(Wahlstatistik[2]))
+
+# Get data
